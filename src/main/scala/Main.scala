9bd4e77f23fb700fbbf2107e7e9d32d8d7ea0faf
Samy Dindane <samy@dindane.com>
2015-06-11 21:49:54 +0200

    Implement a basic algorithm (without treasures counting)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,4 @@
 object Main {
-  def main(args: Array[String]) = {
-    val configFile = args.head
-    val configuration = scala.io.Source.fromFile(configFile).getLines.mkString("\n")
-    val game = parseConfiguration(configuration)
-  }
-
   object Orientation extends Enumeration {
     type Orientation = Value
     val North = Value("N")
@@ -17,35 +11,111 @@ object Main {
   object Move extends Enumeration {
     type Move = Value
     val Forward = Value("A")
-    val Right = Value("D")
-    val Left = Value("G")
+    val Right   = Value("D")
+    val Left    = Value("G")
   }
   import Move._
 
   case class Game(map: Map, treasures: Seq[Treasure], mountains: Seq[Mountain], players: Seq[Player])
-  case class Map(width: Long, height: Long)
+  case class Map(width: Int, height: Int)
   case class Position(x: Long, y: Long)
   case class Treasure(position: Position, quantity: Long)
   case class Mountain(position: Position)
-  case class Player(name: String, position: Position, orientation: Orientation, moves: List[Move], movesCounter: Long)
+  case class Player(name: String, position: Position, orientation: Orientation, moves: List[Move],
+    movesCounter: Int, treasures: Long)
+
+  def main(args: Array[String]): Unit = {
+    val configFile = args.head
+    val configuration = scala.io.Source.fromFile(configFile).getLines.mkString("\n")
+    val game = parseConfiguration(configuration)
+
+    start(game)
+  }
+
+  def moveForward(orientation: Orientation, position: Position, game: Game): Position = {
+    val (x, y) = orientation match {
+      case Orientation.North => (position.x, position.y - 1)
+      case Orientation.East => (position.x + 1, position.y)
+      case Orientation.South => (position.x, position.y + 1)
+      case Orientation.West => (position.x - 1, position.y)
+    }
+
+    val newX = if (x < 0) 0
+    else if (x > game.map.width - 1) game.map.width - 1
+    else x
+
+    val newY = if (y < 0) 0
+    else if (y > game.map.height - 1) game.map.height - 1
+    else y
+
+    game.mountains.find(_.position == Position(newX, newY))
+      .map(_ => position)
+      .getOrElse(Position(newX, newY))
+  }
+
+  def turn(orientation: Orientation, move: Move): Orientation = {
+    val o = Seq(North, East, South, West)
+
+    def predecessor(orientation: Orientation) = {
+      o((o.indexOf(orientation) + 3) % 4)
+    }
+
+    def successor(orientation: Orientation) = {
+      o((o.indexOf(orientation) + 1) % 4)
+    }
+
+    move match {
+      case Right => successor(orientation)
+      case Left  => predecessor(orientation)
+      case _     => throw new Exception("This kind of move shouldn't be there.") // bhoo
+    }
+  }
+
+  def start(game: Game): Game = {
+    def ƒ(game: Game): Game = {
+      val player = game.players.head
+      if (player.movesCounter == player.moves.size) {
+        game
+      } else {
+        val (position, orientation) =
+          if (player.moves(player.movesCounter) == Move.Forward) {
+            val newPosition = moveForward(player.orientation, player.position, game)
+            val newOrientation = player.orientation
+            (newPosition, newOrientation)
+          } else {
+            val newPosition = player.position
+            val newOrientation: Orientation = turn(player.orientation, player.moves(player.movesCounter))
+            (newPosition, newOrientation)
+          }
+
+        val newPlayer = player.copy(position = position,
+          orientation = orientation,
+          movesCounter = player.movesCounter + 1)
+
+        ƒ(game.copy(players = List(newPlayer)))
+      }
+    }
+
+    ƒ(game)
+  }
 
   def parseConfiguration(config: String): Game = {
     val splitConfig = config.split("\n")
 
     val mapLine = splitConfig.filter(_.startsWith("C ")).head
-    val map = Map(width = mapLine.split(" ")(1).toLong, height = mapLine.split(" ")(2).toLong)
+    val map = Map(width = mapLine.split(" ")(1).toInt, height = mapLine.split(" ")(2).toInt)
 
     val treasures: Seq[Treasure] = splitConfig.filter(_.startsWith("T ")).map { line =>
       val splitLine = line.split(" ")
-      val position = Position(x = splitLine(1).split("-")(0).toLong,
-        y = splitLine(1).split("-")(1).toLong)
+      val position = Position(x = splitLine(1).split("-")(0).toLong - 1,
+        y = splitLine(1).split("-")(1).toLong - 1)
       Treasure(position, splitLine(2).toLong)
     }
 
     val mountains: Seq[Mountain] = splitConfig.filter(_.startsWith("M ")).map { line =>
       val splitLine = line.split(" ")
-      val position = Position(x = splitLine(1).split("-")(0).toLong,
-        y = splitLine(1).split("-")(1).toLong)
+      val position = Position(x = splitLine(1).split("-")(0).toLong - 1,
+        y = splitLine(1).split("-")(1).toLong - 1)
       Mountain(position)
     }
 
@@ -55,11 +125,11 @@ object Main {
       .map { line =>
         val splitLine = line.split(" ")
         val name = splitLine(4)
-        val position = Position(x = splitLine(1).split("-")(0).toLong,
-          y = splitLine(1).split("-")(1).toLong)
+        val position = Position(x = splitLine(1).split("-")(0).toLong - 1,
+          y = splitLine(1).split("-")(1).toLong - 1)
         val orientation = Orientation.withName(splitLine(2))
         val moves: List[Move] = splitLine(3).map(c => Move.withName(c.toString)).toList
-        Player(name, position, orientation, moves, movesCounter = 0)
+        Player(name, position, orientation, moves, movesCounter = 0, treasures = 0)
       }.toList
 
     Game(map, treasures, mountains, players)
--- src/test/scala/TestSpec.scala
+++ src/test/scala/TestSpec.scala
@@ -5,16 +5,17 @@ import Main.Orientation._
 import Main.Move._
 
 class TestSpec extends WordSpec with Matchers {
-  "parseConfiguration" should {
-    lazy val configuration =
-      """
-        |C 6 5
-        |T 4-2 1
-        |T 1-4 3
-        |M 5-3
-        |John 1-1 E AADADAGA Bunny
-      """.stripMargin
+  lazy val configuration =
+    """
+      |C 6 5
+      |T 1-1 1
+      |T 3-2 3
+      |M 5-3
+      |M 6-5
+      |John 1-1 E AADADAGA Bunny
+    """.stripMargin
 
+  "parseConfiguration" should {
     "return a map" in {
       val game = parseConfiguration(configuration)
       game.map.width shouldBe 6
@@ -24,30 +25,121 @@ class TestSpec extends WordSpec with Matchers {
     "return a list of treasures" in {
       val game = parseConfiguration(configuration)
       game.treasures.size shouldBe 2
-      game.treasures.head.position.x shouldBe 4
-      game.treasures.head.position.y shouldBe 2
+      game.treasures.head.position.x shouldBe 0
+      game.treasures.head.position.y shouldBe 0
       game.treasures.head.quantity shouldBe 1
-      game.treasures(1).position.x shouldBe 1
-      game.treasures(1).position.y shouldBe 4
+      game.treasures(1).position.x shouldBe 2
+      game.treasures(1).position.y shouldBe 1
       game.treasures(1).quantity shouldBe 3
     }
 
     "return a list of mountains" in {
       val game = parseConfiguration(configuration)
-      game.mountains.size shouldBe 1
-      game.mountains.head.position.x shouldBe 5
-      game.mountains.head.position.y shouldBe 3
+      game.mountains.size shouldBe 2
+      game.mountains.head.position.x shouldBe 4
+      game.mountains.head.position.y shouldBe 2
+      game.mountains(1).position.x shouldBe 5
+      game.mountains(1).position.y shouldBe 4
     }
 
     "return a list of players" in {
       val game = parseConfiguration(configuration)
       game.players.size shouldBe 1
       game.players.head.name shouldBe "Bunny"
-      game.players.head.position.x shouldBe 1
-      game.players.head.position.y shouldBe 1
+      game.players.head.position.x shouldBe 0
+      game.players.head.position.y shouldBe 0
       game.players.head.orientation shouldBe East
       game.players.head.moves shouldBe List(Forward, Forward, Right, Forward, Right, Forward, Left, Forward)
       game.players.head.movesCounter shouldBe 0
+      game.players.head.treasures shouldBe 0
+    }
+  }
+
+  "start" should {
+    "return a new players' list" in {
+      val game = start(parseConfiguration(configuration))
+      game.players.size shouldBe 1
+      game.players.head.position.x shouldBe 1
+      game.players.head.position.y shouldBe 2
+      game.players.head.orientation shouldBe South
+      game.players.head.movesCounter shouldBe 8
+//      game.players.head.treasures shouldBe 4
+    }
+  }
+
+  "moveFoward" should {
+    lazy val game = parseConfiguration(configuration)
+
+    "move to the north" in {
+      val position = moveForward(North, Position(3, 2), game)
+      position.x shouldBe 3
+      position.y shouldBe 1
+    }
+
+    "move to the east" in {
+      val position = moveForward(East, Position(4, 3), game)
+      position.x shouldBe 5
+      position.y shouldBe 3
+    }
+
+    "move to the south" in {
+      val position = moveForward(South, Position(3, 2), game)
+      position.x shouldBe 3
+      position.y shouldBe 3
+    }
+
+    "move to the west" in {
+      val position = moveForward(West, Position(3, 2), game)
+      position.x shouldBe 2
+      position.y shouldBe 2
+    }
+
+    "don't move outside of the map" in {
+      {
+        val position = moveForward(North, Position(0, -1000000), game)
+        position.x shouldBe 0
+        position.y shouldBe 0
+      }
+
+      {
+        val position = moveForward(North, Position(-1000000, 0), game)
+        position.x shouldBe 0
+        position.y shouldBe 0
+      }
+
+      {
+        val position = moveForward(North, Position(0, 1000000), game)
+        position.x shouldBe 0
+        position.y shouldBe game.map.height - 1
+      }
+
+      {
+        val position = moveForward(North, Position(1000000, 0), game)
+        position.x shouldBe game.map.width - 1
+        position.y shouldBe 0
+      }
+    }
+
+    "don't move where a mountain is" in {
+      val position = moveForward(South, Position(5, 3), game)
+      position.x shouldBe 5
+      position.y shouldBe 3
+    }
+  }
+
+  "turn" should {
+    "turn to the left" in {
+      turn(North, Left) shouldBe West
+      turn(East,  Left) shouldBe North
+      turn(South, Left) shouldBe East
+      turn(West,  Left) shouldBe South
+    }
+
+    "turn to the right" in {
+      turn(North, Right) shouldBe East
+      turn(East,  Right) shouldBe South
+      turn(South, Right) shouldBe West
+      turn(West,  Right) shouldBe North
     }
   }
 }
