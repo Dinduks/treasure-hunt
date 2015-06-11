f91ca5ffa2be502c839314eeba9e3076dc1c78b8
Samy Dindane <samy@dindane.com>
2015-06-11 17:05:53 +0200

    Parse config files + Add the Game class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,36 @@
-object Main extends App {
-  println("Hello world")
+object Main {
+  def main(args: Array[String]) = {
+    val configFile = args.head
+    val configuration = scala.io.Source.fromFile(configFile).getLines.mkString("\n")
+    val game = parseConfiguration(configuration)
+  }
+
+  case class Game(map: Map, treasures: Seq[Treasure], mountains: Seq[Mountain])
+  case class Map(width: Long, height: Long)
+  case class Position(x: Long, y: Long)
+  case class Treasure(position: Position, quantity: Long)
+  case class Mountain(position: Position)
+
+  def parseConfiguration(config: String): Game = {
+    val splitConfig = config.split("\n")
+
+    val mapLine = splitConfig.filter(_.startsWith("C ")).head
+    val map = Map(width = mapLine.split(" ")(1).toLong, height = mapLine.split(" ")(2).toLong)
+
+    val treasures: Seq[Treasure] = splitConfig.filter(_.startsWith("T ")).map { line =>
+      val splitLine = line.split(" ")
+      val position = Position(x = splitLine(1).split("-")(0).toLong,
+        y = splitLine(1).split("-")(1).toLong)
+      Treasure(position, splitLine(2).toLong)
+    }
+
+    val mountains: Seq[Mountain] = splitConfig.filter(_.startsWith("M ")).map { line =>
+      val splitLine = line.split(" ")
+      val position = Position(x = splitLine(1).split("-")(0).toLong,
+        y = splitLine(1).split("-")(1).toLong)
+      Mountain(position)
+    }
+
+    Game(map, treasures, mountains)
+  }
 }
--- src/test/scala/TestSpec.scala
+++ src/test/scala/TestSpec.scala
@@ -1,12 +1,39 @@
 
 import org.scalatest._
+import Main._
 
-class TestSpec extends WordSpec with MustMatchers {
+class TestSpec extends WordSpec with Matchers {
+  "parseConfiguration" should {
+    lazy val configuration =
+      """
+        |C 6 5
+        |T 4-2 1
+        |T 1-4 3
+        |M 5-3
+      """.stripMargin
 
-  "test" should {
-    "test function" in {
-      pending
+    "return a map" in {
+      val game = parseConfiguration(configuration)
+      game.map.width shouldBe 6
+      game.map.height shouldBe 5
+    }
+
+    "return a list of treasures" in {
+      val game = parseConfiguration(configuration)
+      game.treasures.size shouldBe 2
+      game.treasures.head.position.x shouldBe 4
+      game.treasures.head.position.y shouldBe 2
+      game.treasures.head.quantity shouldBe 1
+      game.treasures(1).position.x shouldBe 1
+      game.treasures(1).position.y shouldBe 4
+      game.treasures(1).quantity shouldBe 3
+    }
+
+    "return a list of mountains" in {
+      val game = parseConfiguration(configuration)
+      game.mountains.size shouldBe 1
+      game.mountains.head.position.x shouldBe 5
+      game.mountains.head.position.y shouldBe 3
     }
   }
-
 }
